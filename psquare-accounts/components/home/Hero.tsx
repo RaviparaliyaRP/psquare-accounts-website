586c98ddb15cef8a32c329590007ea38d074abba
PARALIYA RAVI <psquaregst@gmail.com>
2025-10-21 10:30:57 +0530

    Fix hero background image path - Change back to /hero-bg.jpeg (root of public folder)

--- psquare-accounts/components/home/Hero.tsx
+++ psquare-accounts/components/home/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <Image
-          src="/images/hero-bg.jpeg"
+          src="/hero-bg.jpeg"
           alt="Hero Background"
           fill
           className="object-cover opacity-80"
